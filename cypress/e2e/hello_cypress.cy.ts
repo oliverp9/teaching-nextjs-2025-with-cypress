a1e880fba20d4b7f834d9a7d0dc7f4119bb1e2da
OLIMINATOR <oliverplassiard@proton.me>
2025-11-02 18:05:16 +0100

    first 3 hello tests

--- cypress/e2e/hello_cypress.cy.ts
+++ cypress/e2e/hello_cypress.cy.ts
@@ -1,19 +1,38 @@
-describe('Album Catalog - Basic Checks', () => {
-  it('opens the homepage', () => {
-    cy.visit('/');
+describe("Album Catalog - Basic Checks", () => {
+    it("opens the homepage", () => {
+        cy.visit("/");
 
-    // make this test pass by adding the correct attribute data-cy into your page
-    cy.get('[data-cy="title"]').should('be.visible');
-    cy.get('[data-cy="title"]').should('contain.text', 'Spotify');
-  });
+        // make this test pass by adding the correct attribute data-cy into your page
+        cy.get('[data-cy="title"]').should("be.visible");
+        cy.get('[data-cy="title"]').should("contain.text", "Spotify");
+    });
 
-  it('displays the site title in the header', () => {});
+    it("displays the site title in the header", () => {
+        cy.visit("/");
 
-  it('shows at least one album card', () => {});
+        cy.get('[data-cy="site-link"]').should("be.visible");
+        cy.get('[data-cy="site-link"]').should("contain.text", "Spotify");
+    });
 
-  it('album card has a title and price', () => {});
+    it("shows at least one album card", () => {
+        cy.visit("/");
 
-  it('has a visible search input on the top', () => {});
+        cy.get('[data-cy="album-card"]').should("have.length.at.least", 1);
+    });
 
-  // add at least 3 more tests here
+    it("album card has a title and price", () => {
+        cy.visit("/");
+
+        cy.get('[data-cy="album-title"]').should("be.visible");
+
+        // price?
+    });
+
+    it("has a visible search input on the top", () => {
+        cy.visit("/");
+
+        cy.get('[data-cy="search-input"]').should("be.visible");
+    });
+
+    // add at least 3 more tests here
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,57 +2,73 @@ import { getDb } from "@/lib/db";
 import Link from "next/link";
 
 export default async function Home() {
-  const db = getDb();
+    const db = getDb();
 
-  const albums = await db
-    .selectFrom("albums")
-    .innerJoin("authors", "albums.author_id", "authors.id")
-    .select([
-      "albums.id",
-      "albums.name",
-      "albums.release_date",
-      "authors.name as author_name",
-      "authors.id as author_id",
-    ])
-    .execute();
+    const albums = await db
+        .selectFrom("albums")
+        .innerJoin("authors", "albums.author_id", "authors.id")
+        .select([
+            "albums.id",
+            "albums.name",
+            "albums.release_date",
+            "authors.name as author_name",
+            "authors.id as author_id",
+        ])
+        .execute();
 
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p className="text-4xl font-bold">Spotify</p>
-        <div className="grid grid-cols-2 gap-4">
-          {albums.map((album) => (
-            <div key={album.id} className="card w-64 bg-base-100 shadow-sm">
-              <div className="card-body">
-                <span className="badge badge-xs badge-warning">Pop</span>
-                <h2 className="text-3xl font-bold">{album.name}</h2>
-
-                <p>ID: {album.id}</p>
-                <p>
-                  Author:{" "}
-                  <Link href={`/author/${album.author_id}`}>
-                    {album.author_name}
-                  </Link>
+    return (
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <p className="text-4xl font-bold" data-cy="title">
+                    Spotify
                 </p>
-                <p>
-                  Release Date: {new Date(album.release_date).toDateString()}
-                </p>
-                <div className="mt-6">
-                  <Link
-                    className="btn btn-primary btn-block"
-                    href={`/album/${album.id}`}
-                  >
-                    Detail
-                  </Link>
+                <div className="grid grid-cols-2 gap-4">
+                    {albums.map((album) => (
+                        <div
+                            key={album.id}
+                            className="card w-64 bg-base-100 shadow-sm"
+                            data-cy="album-card"
+                        >
+                            <div className="card-body">
+                                <span className="badge badge-xs badge-warning">
+                                    Pop
+                                </span>
+                                <h2
+                                    className="text-3xl font-bold"
+                                    data-cy="album-title"
+                                >
+                                    {album.name}
+                                </h2>
+
+                                <p>ID: {album.id}</p>
+                                <p>
+                                    Author:{" "}
+                                    <Link href={`/author/${album.author_id}`}>
+                                        {album.author_name}
+                                    </Link>
+                                </p>
+                                <p>
+                                    Release Date:{" "}
+                                    {new Date(
+                                        album.release_date
+                                    ).toDateString()}
+                                </p>
+                                <div className="mt-6">
+                                    <Link
+                                        className="btn btn-primary btn-block"
+                                        href={`/album/${album.id}`}
+                                    >
+                                        Detail
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-              </div>
-            </div>
-          ))}
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                <p>Footer</p>
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>Footer</p>
-      </footer>
-    </div>
-  );
+    );
 }
